e510eab209c84951a2a578bead0693ce45441305
somu146 <somnath.pansare@neosoftmail.com>
2023-04-13 17:34:16 +0530

    Setup project

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link lib/animate/animate.min.css
+//= link lib/owlcarousel/assets/owl.carousel.min.css
